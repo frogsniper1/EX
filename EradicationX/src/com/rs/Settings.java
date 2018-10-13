53ee8c96453f84d0198cbbbe4e1119dfc9de3ce3
DESKTOP-U7ORJKE\kenneth <kennethpapango40@gmail.com>
2018-10-13 16:28:47 +0800

    testing

--- EradicationX/src/com/rs/Settings.java
+++ EradicationX/src/com/rs/Settings.java
@@ -8,7 +8,7 @@ import com.rs.rss.RSSFeedParser;
 
 public final class Settings {
 
-	public static final String SERVER_NAME = "EradicationX - Test 2";
+	public static final String SERVER_NAME = "EradicationX - Testing lang";
 	public static final String OWNER = "Era,Vlad,Jenny";		
 	public static final int PORT_ID = 43594;
 	public static final int CLIENT_BUILD = 718;
